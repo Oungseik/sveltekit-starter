6121754f1749a41da40bc2be58cf5ed753e615c4
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2025-10-23 10:17:21 +0630

    feat: setup svelte with convex

--- apps/website/src/routes/+layout.svelte
+++ apps/website/src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
   import "@fontsource/inter";
   import { Toaster } from "@repo/ui/sonner";
+  import { setupConvex } from "convex-svelte";
   import { ModeWatcher } from "mode-watcher";
+
+  import { PUBLIC_CONVEX_URL } from "$env/static/public";
 
   import "../app.css";
   import type { LayoutProps } from "./$types";
 
+  setupConvex(PUBLIC_CONVEX_URL);
   let { children }: LayoutProps = $props();
 </script>
 
