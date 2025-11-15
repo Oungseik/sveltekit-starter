5c517ae728813c5e14134485b0377e909f0e731d
Oung Seik <mhemaungthuwin@gmail.com>
2025-11-15 13:50:39 +0630

    chore: remove convex

--- apps/website/src/routes/+layout.svelte
+++ apps/website/src/routes/+layout.svelte
@@ -1,15 +1,11 @@
 <script lang="ts">
   import "@fontsource/inter";
   import { Toaster } from "@repo/ui/sonner";
-  import { setupConvex } from "convex-svelte";
   import { ModeWatcher } from "mode-watcher";
-
-  import { PUBLIC_CONVEX_URL } from "$env/static/public";
 
   import "../app.css";
   import type { LayoutProps } from "./$types";
 
-  setupConvex(PUBLIC_CONVEX_URL);
   let { children }: LayoutProps = $props();
 </script>
 
